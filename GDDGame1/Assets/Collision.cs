553ba0d7ea480827c174fa9eeb8a0d1fe9e4e5e4
Vizualyse <50756314+Vizualyse@users.noreply.github.com>
2019-10-31 16:25:01 +0000

    Fixed Invul
    Improved Invulnerability Script
    Stopped the dog from chasing while character is flashing

--- GDDGame1/Assets/Collision.cs
+++ GDDGame1/Assets/Collision.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Collision : MonoBehaviour
 {
@@ -8,7 +9,8 @@ public class Collision : MonoBehaviour
 
     public GameObject hud;
     private bool invincible;
-    private float invincibilityTime = 10.0f;
+    private float invincibilityTime = 3.0f;
+    public int flashingSpeed;
     void Start()
     {
         
@@ -17,21 +19,45 @@ public class Collision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(invincible);
     }
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy") && !invincible)
         {
             hud.GetComponent<Heart_Counter>().health-- ;
-            Invulnerability();
+            StartCoroutine(Invulnerability(collision.gameObject));
+            StartCoroutine(redColour());
+            StartCoroutine(flashing());
         }
     }
 
-    IEnumerator Invulnerability()
+    IEnumerator Invulnerability(GameObject enemy)
     {
         invincible = true;
+        enemy.GetComponent<AICharacterControl>().enabled = false;
+        enemy.GetComponent<ThirdPersonCharacter>().Move(Vector3.zero, false, false);
         yield return new WaitForSeconds(invincibilityTime);
+        enemy.GetComponent<AICharacterControl>().enabled = true;
         invincible = false;
+    }
+
+    IEnumerator redColour()
+    {
+        this.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        yield return new WaitForSeconds(invincibilityTime);
+        this.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+    }
+
+    IEnumerator flashing()
+    {
+        for (int i = 0; i < flashingSpeed; i++)
+        {
+            this.transform.GetChild(0).gameObject.GetComponent<SkinnedMeshRenderer>().gameObject.SetActive(false);
+            yield return new WaitForSeconds(invincibilityTime / (2*flashingSpeed));
+            this.transform.GetChild(0).gameObject.GetComponent<SkinnedMeshRenderer>().gameObject.SetActive(true);
+            yield return new WaitForSeconds(invincibilityTime / (2 * flashingSpeed));
+        }
+
     }
 }
